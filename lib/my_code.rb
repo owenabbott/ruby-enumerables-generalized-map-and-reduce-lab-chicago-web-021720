afdc00c8fb14dc808fe13fd80baab90ab95bf67b
Owen Abbott <ocabbott@gmail.com>
2020-02-04 20:58:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,6 +20,8 @@ end
 #end
 # saving this code because I'm not sure why it didn't work and might try to revive it later after going through the supplied video walkthrough.
 
+#upon going through the video walkthrough I'm more confused. My 'for' loop does the same thing their 'while' loop does. Why doesn't it work?
+
 
 
 def reduce(array, starting_point = 0)
